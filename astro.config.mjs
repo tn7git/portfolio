180496cc555dc2cc1ec2cf9cae60eeeff09fbe03
tim-brownink <tim@brownink.com.au>
2023-08-31 14:25:33 +1000

    first push

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://tn7git.github.io',
-    base: '/test-astro',
+    base: '/portfolio',
 
 
 });
